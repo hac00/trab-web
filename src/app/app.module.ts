a593bed8c3e27b38d3771551ec5cb606bfe6a7ce
Henriq <henriqueac00.gmail.com>
2025-07-10 02:30:57 -0300

    footer: done, homepage: structure done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { GameSliderComponent } from './components/game-slider/game-slider.component';
+import { PopularReviewsComponent } from './components/popular-reviews/popular-reviews.component';
+import { PopularReviewsListComponent } from './components/popular-reviews-list/popular-reviews-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomepageComponent
+    HomepageComponent,
+    CarouselComponent,
+    GameSliderComponent,
+    PopularReviewsComponent,
+    PopularReviewsListComponent
   ],
   imports: [
     BrowserModule,
