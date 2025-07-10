17b733123c822636577cda90f5019a68df2557b0
Henriq <henriqueac00.gmail.com>
2025-07-10 04:02:09 -0300

    game.service.ts: created

--- src/app/models/game.ts
+++ src/app/models/game.ts
@@ -5,16 +5,14 @@ export class Game {
   comments: string;
   imageUrl?: string;
   releaseDate: Date;
-  genre: string;
   platform: string;
   hoursPlayed?: number;
 
-  constructor(name: string, status: string, comments: string, releaseDate: Date, genre: string, platform: string, id?: number, imageUrl?: string, hoursPlayed?: number) {    
+  constructor(name: string, status: string, comments: string, releaseDate: Date, platform: string, id?: number, imageUrl?: string, hoursPlayed?: number) {    
     this.name = name;
     this.status = status;
     this.comments = comments;
     this.releaseDate = releaseDate;
-    this.genre = genre;
     this.platform = platform;
     this.id = id;
     this.imageUrl = imageUrl;
---
+++ src/app/services/game.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Game } from '../models/game';
+import Dexie from 'dexie';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService extends Dexie{
+
+  games: Dexie.Table<Game, number>;
+
+  constructor() {
+    super('GameDB');
+    this.version(1).stores({
+      games: '++id, name, status, platform, releaseDate, imageUrl, comments, hoursPlayed'
+    });
+    this.games = this.table('games');
+  }
+
+  async addGame(game: Game): Promise<number> {
+    return await this.games.add(game);
+  }
+
+  async getGames(): Promise<Game[]> {
+    return await this.games.toArray();
+  }
+
+  async updateGame(id: number, game: Game): Promise<number> {
+    return await this.games.update(id, game);
+  }
+
+  async deleteGame(id: number): Promise<void> {
+    return await this.games.delete(id);
+  }
+}
